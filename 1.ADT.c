7624234610e4b24ca0e533ddb2ebda64b1b1434d
aka7898 <132600421+aka7898@users.noreply.github.com>
2023-12-27 08:47:36 +0530

    This is all about ABSTRACT DATA TYPE.

---
+++ 1.ADT.c
@@ -0,0 +1,10 @@
+/* Abstract Data Type
+
+It is a blueprint of Minimal Requirements and Operations 
+
+        SET OF VALUES   +   SET OF OPERATIONS
+
+Details are abstracted of the operation
+
+As, if we add 9+12 the result we get will be 21, and didn't 
+get how the numbers are added this is ABSTRACTION 
