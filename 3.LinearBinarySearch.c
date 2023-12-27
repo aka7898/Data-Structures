ae7c0d4c146924a49587e8beda9836c498a95faa
aka7898 <132600421+aka7898@users.noreply.github.com>
2023-12-27 10:03:24 +0530

    THIS IS ALL ABOUT LINEAR AND BINARY SEARCH IN THE DATA STRUCTURES AND ALGORITHM IN C.

---
+++ 3.LinearBinarySearch.c
@@ -0,0 +1,24 @@
+/*
+                                         Linear Seach
+Linear Search       
+                traversing and checking each element of the array
+
+
+COMPLEXITY      -       O(n)   
+
+
+Binary Seach       
+                arranging into ascending, then the first element will be low and last will be high, if the element we are searching
+for is neither high nor low then we will find the mid element i.e. (high+low)/2 and then checking the mid value whether it is the element
+we are seaching for, if not then compare the element we are searching for with the mid value and then the element we are seaching for is 
+smaller then the mid value then will be making mid value to high,   and if the element we are searching for is greater then the mid value then
+we will be taking the mid valye as the low, and traversing to find the element.... and so on..... 
+
+        e.g.    if we are given a book of about 1000pages and we have to search for page 456, then we will take to mid page of the book and the 
+                then comparing is it smaller or bigger than the mid page.
+
+a.      sorted array
+
+
+COMPLEXITY      -       O(logn)
+*/
